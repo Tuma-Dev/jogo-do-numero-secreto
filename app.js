ccd23b814e2e6979adee515a7ab55d18b620da70
Felippe Tuma <69515195+Tuma-Dev@users.noreply.github.com>
2025-02-02 11:46:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-let listNumbersSorteados = []; // criando uma lista vazia.
+let listNumbersSorteados = [];
 let numeroLimite = 10;
 let numberSecret = generateNumber();
 let tentativas = 1;
 
-// função para modificar os campos, com parametos de tag e texto
 function displayText(tag, text) {
     let camp = document.querySelector(tag);
     camp.innerHTML = text;
@@ -11,9 +10,7 @@ function displayText(tag, text) {
 }
 
 function displayMessageInitial() {
-    // puxando função e paramentos de cada compo
     displayText('h1', 'Jogo do Número Secreto');
-    // let messageEscolhaDeNumeros = `Escolha um número entre 1 e ${numeroLimite}:`
     displayText('p', 'Escolha um número entre 1 e 10:');
 }
 
@@ -38,17 +35,16 @@ function verificarChute() {
     console.log(numberSecret);
 }
 
-// return para retornar para o codigo
 function generateNumber() {
     let numberEscolhido = parseInt(Math.random() * numeroLimite + 1);
     let quantidadeDeElementosDaLista = listNumbersSorteados.length;
     if(quantidadeDeElementosDaLista == numeroLimite) {
         listNumbersSorteados = [];
     }
-    if(listNumbersSorteados.includes(numberEscolhido)) { //includes verifica se o elemento está na lista
+    if(listNumbersSorteados.includes(numberEscolhido)) {
         return generateNumber();
     } else {
-        listNumbersSorteados.push(numberEscolhido);//push para colocar na lista no final da lista.
+        listNumbersSorteados.push(numberEscolhido);
         console.log(listNumbersSorteados);
         return numberEscolhido;
     }
